f50eadd8e130d9452d8b0bb91a1a1e8f788f7e0b
kelllllllly <kellyveintimilla1@gmail.com>
2025-02-16 22:01:02 -0500

    updated line 103, and 83 with same issue of inbuffer

--- client.cpp
+++ client.cpp
@@ -80,7 +80,7 @@ int main (int argc, char** argv) // to include cmd line arguments
         sprintf(in_buffer, "%.2f", client_temp); // stores my_temp into in_buffer
         // Send message to server
 		//  Data sent is the client's message queue name
-        if (mq_send (qd_server, in_buffer , strlen(inbuffer), 0) == -1) {
+        if (mq_send (qd_server, in_buffer , strlen(in_buffer), 0) == -1) {
              cerr<<"Client: Not able to send message to server";
             continue;
         }
@@ -100,7 +100,7 @@ int main (int argc, char** argv) // to include cmd line arguments
             break;
         }
         // calculate new cent temp after server calculates new temp 
-        float central_temp = atof(inbuffer); 
+        float central_temp = atof(in_buffer); 
         client_temp = (client_temp * 3 + 3 * central_temp) / 5;
         printf("client %d updated temperature: %.2f\n", getpid(), client_temp); 
     }
