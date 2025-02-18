cadb5025b2df6f3af3a17bbd8f6e2792d6cec58d
kelllllllly <kellyveintimilla1@gmail.com>
2025-02-18 10:34:27 -0500

    fixiubng formatting + stablize

--- client.cpp
+++ client.cpp
@@ -1,7 +1,7 @@
 /*
- * Adapted from  Softpryaog 
-  *    https://www.softprayog.in/programming/interprocess-communication-using-posix-message-queues-in-linux
-  * by MA Doman 2018
+ * Adapted from  Softpryaog
+ *    https://www.softprayog.in/programming/interprocess-communication-using-posix-message-queues-in-linux
+ * by MA Doman 2018
  * client.cpp: Client program
  *           to demonstrate interprocess communication
  *           with POSIX message queues
@@ -19,97 +19,104 @@
 #include <iostream>
 
 // DEFINE THE SERVER NAME AN DEFAULT VALUES FOR THE MESSAGE QUEUE
-#define SERVER_QUEUE_NAME   "/kellyserver"
-#define QUEUE_PERMISSIONS 0660  // like chmod values, user and owner can read and write to queue
+#define SERVER_QUEUE_NAME "/kellyserver"
+#define QUEUE_PERMISSIONS 0660 // like chmod values, user and owner can read and write to queue
 #define MAX_MESSAGES 10
 #define MAX_MSG_SIZE 256
-#define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10   // leave some extra space after message
+#define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10 // leave some extra space after message
 
 using namespace std;
 // resources used:
-// https://www.w3schools.com/c/c_strings.php - for string functions 
-// https://www.w3schools.com/c/ref_stdio_sprintf.php 
-// https://en.cppreference.com/w/cpp/language/reinterpret_cast 
+// https://www.w3schools.com/c/c_strings.php - for string functions
+// https://www.w3schools.com/c/ref_stdio_sprintf.php
+// https://en.cppreference.com/w/cpp/language/reinterpret_cast
 // https://www.softprayog.in/programming/interprocess-communication-using-posix-message-queues-in-linux
-// https://man7.org/linux/man-pages/man7/mq_overview.7.html  
+// https://man7.org/linux/man-pages/man7/mq_overview.7.html
 // https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/MQueues.html ** helped understandning with struct passing!
 
-struct send_to_server{
+struct send_to_server
+{
     char client_queue_name[64];
-    float client_temp; 
+    float client_temp;
 };
 
-int main (int argc, char** argv) // to include cmd line arguments 
+int main(int argc, char **argv) // to include cmd line arguments
 {
-    if (argc < 3 ){ // makes sure that the user provides the correct amount of command line arguments
+    if (argc < 3)
+    { // makes sure that the user provides the correct amount of command line arguments
         cerr << "usage: ./client (intial temp) (client id: (1-4))\n";
         exit(1);
     }
-    mqd_t qd_server, qd_client;   // queue descriptors
-    send_to_server msg; // will be used to store messages btwn client and server
+    mqd_t qd_server, qd_client; // queue descriptors
+    send_to_server msg;         // will be used to store messages btwn client and server
 
     // creates queue name using client id and is passed as rgument
     string str_client_queue_name = "/kellyclient-" + to_string(atoi(argv[2]));
-	strcpy(msg.client_queue_name, str_client_queue_name.c_str());
-    
-    // client temp 
-    msg.client_temp = atof(argv[1]); // turns string to float 
+    strcpy(msg.client_queue_name, str_client_queue_name.c_str());
+
+    // client temp
+    msg.client_temp = atof(argv[1]);                 // turns string to float
     float prev_client_temperature = msg.client_temp; // will store the intial client temp
 
     // Build message queue attribute structure passed to the mq open
     struct mq_attr attr;
-		attr.mq_flags = 0;
-		attr.mq_maxmsg = MAX_MESSAGES;
-		attr.mq_msgsize = sizeof(send_to_server);
-		attr.mq_curmsgs = 0;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = MAX_MESSAGES;
+    attr.mq_msgsize = sizeof(send_to_server);
+    attr.mq_curmsgs = 0;
 
-	bool up = true; 
-
-	// Create and open client message queue
-    if ((qd_client = mq_open (msg.client_queue_name, O_CREAT | O_RDONLY, QUEUE_PERMISSIONS, &attr)) == -1) {
-        cerr<<"Client: mq_open (client)";
-        exit (1);
+    // Create and open client message queue
+    if ((qd_client = mq_open(msg.client_queue_name, O_CREAT | O_RDONLY, QUEUE_PERMISSIONS, &attr)) == -1)
+    {
+        cerr << "Client: mq_open (client)";
+        exit(1);
     }
-	cout << "Client created queue: " << msg.client_queue_name << endl;
+    cout << "Client created queue: " << msg.client_queue_name << endl;
 
-	// Open server message queue. The name is shared. It is opend write and read only
-    if ((qd_server = mq_open (SERVER_QUEUE_NAME, O_WRONLY)) == -1) {
-         cerr<<"Client: mq_open (server)";
-        exit (1);
+    // Open server message queue. The name is shared. It is opend write and read only
+    if ((qd_server = mq_open(SERVER_QUEUE_NAME, O_WRONLY)) == -1)
+    {
+        cerr << "Client: mq_open (server)";
+        exit(1);
     }
-    
-    while (up) {
+
+    bool up = true;
+
+    while (up)
+    {
         // Send message to server
-		//  Data sent is the client's message queue name
-        if (mq_send (qd_server, reinterpret_cast<char*>(&msg), sizeof(msg), 0) == -1) {
-             cerr<<"Client: Not able to send message to server";
+        //  Data sent is the client's message queue name
+        if (mq_send(qd_server, reinterpret_cast<char *>(&msg), sizeof(msg), 0) == -1)
+        {
+            cerr << "Client: Not able to send message to server";
             continue;
         }
         printf("client %s sent temperature: %.2f\n", msg.client_queue_name, msg.client_temp);
-       
-        // Receive response from server with central temperature 
-        if (mq_receive (qd_client, reinterpret_cast<char*>(&msg), sizeof(msg), NULL) == -1) {
-             cerr<<"Client: mq_receive";
-            exit (1);
+
+        // Receive response from server with central temperature
+        if (mq_receive(qd_client, reinterpret_cast<char *>(&msg), sizeof(msg), NULL) == -1)
+        {
+            cerr << "Client: mq_receive";
+            exit(1);
         }
 
-        // check if client needs to quit 
-        if(msg.client_temp == -1){
+        // check if client needs to quit
+        if (msg.client_temp == -1)
+        {
             cout << "client" << msg.client_queue_name << "quitting.\n";
             up = false;
             break;
         }
-        // calculate new cent temp after server calculates new temp 
-        float central_temp = msg.client_temp;  //stores temp from server
-        msg.client_temp = (prev_client_temperature  * 3 + 2 * central_temp) / 5;
+        // calculate new cent temp after server calculates new temp
+        float central_temp = msg.client_temp; // stores temp from server
+        msg.client_temp = (prev_client_temperature * 3 + 2 * central_temp) / 5;
         prev_client_temperature = msg.client_temp;
-        printf("client %s updated temperature: %.2f\n", msg.client_queue_name, msg.client_temp); 
-
+        printf("client %s updated temperature: %.2f\n", msg.client_queue_name, msg.client_temp);
     }
 
-    // close mq, unlink 
+    // close mq, unlink
     mq_close(qd_client);
     mq_unlink(msg.client_queue_name);
     printf("client %s exiting. \n", msg.client_queue_name);
-    exit (0);
+    exit(0);
 }
--- server.cpp
+++ server.cpp
@@ -16,113 +16,123 @@
 #include <cmath>
 
 // DEFINE THE SERVER NAME AN DEFAULT VALUES FOR THE MESSAGE QUEUE
-#define SERVER_QUEUE_NAME   "/kellyserver"
-#define QUEUE_PERMISSIONS 0660  // like chmod values, user and owner can read and write to queue
+#define SERVER_QUEUE_NAME "/kellyserver"
+#define QUEUE_PERMISSIONS 0660 // like chmod values, user and owner can read and write to queue
 #define MAX_MESSAGES 10
 #define MAX_MSG_SIZE 256
-#define CLIENT_COUNT 4 // update to 4 clients 
+#define CLIENT_COUNT 4 // update to 4 clients
 
 using namespace std;
-// https://www.w3schools.com/c/c_strings.php - for string functions 
-// https://www.w3schools.com/c/ref_stdio_sprintf.php 
-// https://en.cppreference.com/w/cpp/language/reinterpret_cast 
+// https://www.w3schools.com/c/c_strings.php - for string functions
+// https://www.w3schools.com/c/ref_stdio_sprintf.php
+// https://en.cppreference.com/w/cpp/language/reinterpret_cast
 // https://www.softprayog.in/programming/interprocess-communication-using-posix-message-queues-in-linux
-// https://man7.org/linux/man-pages/man7/mq_overview.7.html  
+// https://man7.org/linux/man-pages/man7/mq_overview.7.html
 // https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/MQueues.html ** helped understandning with struct passing!
-struct send_to_server{
-    char client_queue_name[64];
-    float client_temp; 
-};
-int main ()
+struct send_to_server
 {
-    mqd_t qd_server, qd_client[CLIENT_COUNT];   // queue descriptors
-    send_to_server msg; // stores messages between server and client 
+    char client_queue_name[64];
+    float client_temp;
+};
+int main()
+{
+    mqd_t qd_server, qd_client[CLIENT_COUNT]; // queue descriptors
+    send_to_server msg;                       // stores messages between server and client
 
-    float client_temps[CLIENT_COUNT] = {0}; // array that will store recieved client temps 
+    float client_temps[CLIENT_COUNT] = {0}; // array that will store recieved client temps
     float central_temp = 0.0;
-    float prev_temp[CLIENT_COUNT]={0};
+    float prev_temp[CLIENT_COUNT] = {0};
 
-	// Build message queue attribute structure passed to the mq open
+    // Build message queue attribute structure passed to the mq open
     struct mq_attr attr;
 
-		attr.mq_flags = 0;
-		attr.mq_maxmsg = MAX_MESSAGES;
-		attr.mq_msgsize = sizeof(send_to_server);
-		attr.mq_curmsgs = 0;
-	
-    
-        // Open and create the server message queue
-	if ((qd_server = mq_open (SERVER_QUEUE_NAME, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1) {
-        perror ("Server: mq_open (server)");
-        exit (1);
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = MAX_MESSAGES;
+    attr.mq_msgsize = sizeof(send_to_server);
+    attr.mq_curmsgs = 0;
+
+    // Open and create the server message queue
+    if ((qd_server = mq_open(SERVER_QUEUE_NAME, O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr)) == -1)
+    {
+        perror("Server: mq_open (server)");
+        exit(1);
     }
-    cout << "Server: message queue opened!" << endl; 
+    cout << "Server: message queue opened!" << endl;
 
-    bool stabilize = false; // bool for evaluating if the server is stabalized 
+    bool stabilize = false; // bool for evaluating if the server is stabalized
 
-    while(!stabilize){
+    while (!stabilize)
+    {
         float sum_temps = 0.0;
-         // get temps from all clients client 
-         for(int i = 0; i < CLIENT_COUNT; i++){
-            if (mq_receive (qd_server, reinterpret_cast<char*>(&msg), sizeof(msg), NULL) == -1) {
+        // get temps from all clients client
+        for (int i = 0; i < CLIENT_COUNT; i++)
+        {
+            if (mq_receive(qd_server, reinterpret_cast<char *>(&msg), sizeof(msg), NULL) == -1)
+            {
                 cerr << "Server: mq_receive";
-                exit (1);
+                exit(1);
             }
 
-            // store the client temps 
+            // store the client temps
             client_temps[i] = msg.client_temp;
             sum_temps += msg.client_temp;
 
             printf("temperature recieved from client %s: %.2f\n", msg.client_queue_name, msg.client_temp);
-         }
+        }
 
-            // calculates the new central temperature then prints. 
-            float new_cen_temp = (2 * central_temp + sum_temps) / 6.0; // changed back to 6 for original formula
-            printf("new central temperature: %.2f\n", new_cen_temp); 
+        // calculates the new central temperature then prints.
+        float new_cen_temp = (2 * central_temp + sum_temps) / 6.0; // changed back to 6 for original formula
+        printf("new central temperature: %.2f\n", new_cen_temp);
 
-            // stabalization check
-            stabilize = true; 
-            for(int i = 0; i < CLIENT_COUNT; i++ ){
-                if (fabs(client_temps[i] - prev_temp[i]) < 0.01){
-                    stabilize = false;
-                    break;
-                }
+        // stabalization check
+        stabilize = true;
+        for (int i = 0; i < CLIENT_COUNT; i++)
+        {
+            if (fabs(client_temps[i] - prev_temp[i]) >= 0.01)
+            {
+                stabilize = false;
+                break;
             }
-        
-            // send the new central temp back to clients
-            for (int i = 0; i < CLIENT_COUNT; i++) {
-                string client_queue_name = "/kellyclient-" + to_string(i + 1);
-                strcpy(msg.client_queue_name, client_queue_name.c_str());
-             // format new_central_temp to string, then stores it in inbuffer, then send it to client mq  
-             if(stabilize){
+        }
+
+        // send the new central temp back to clients
+        for (int i = 0; i < CLIENT_COUNT; i++)
+        {
+            string client_queue_name = "/kellyclient-" + to_string(i + 1);
+            strcpy(msg.client_queue_name, client_queue_name.c_str());
+            // format new_central_temp to string, then stores it in inbuffer, then send it to client mq
+            if (stabilize)
+            {
                 msg.client_temp = -1;
-            } else{
+            }
+            else
+            {
                 msg.client_temp = new_cen_temp;
             }
 
-                if ((qd_client[i] = mq_open(client_queue_name.c_str(), O_WRONLY)) == -1) {
-                    cerr << "server: mq_open failed for " << client_queue_name << "\n";
-                    continue;
-                }
-                cout << "server: client message queue opened!" << endl;
-
-                mq_send(qd_client[i], reinterpret_cast<char*>(&msg), sizeof(msg), 0);
-                mq_close(qd_client[i]); //close mq
+            if ((qd_client[i] = mq_open(client_queue_name.c_str(), O_WRONLY)) == -1)
+            {
+                cerr << "server: mq_open failed for " << client_queue_name << "\n";
+                continue;
             }
+            cout << "server: client message queue opened!" << endl;
 
-        
-            central_temp = new_cen_temp;   // updates central temperature with the new one
-            for (int i = 0; i < CLIENT_COUNT; i++){
-                prev_temp[i] = client_temps[i]; // prev temp is updarted with new one in array
-        } 
-            }
-            
-	
+            mq_send(qd_client[i], reinterpret_cast<char *>(&msg), sizeof(msg), 0);
+            mq_close(qd_client[i]); // close mq
+        }
+
+        central_temp = new_cen_temp; // updates central temperature with the new one
+        for (int i = 0; i < CLIENT_COUNT; i++)
+        {
+            prev_temp[i] = client_temps[i]; // prev temp is updarted with new one in array
+        }
+    }
+
     printf("system stablized");
 
-    // close server, unlink 
+    // close server, unlink
     mq_close(qd_server);
     mq_unlink(SERVER_QUEUE_NAME);
     printf("exiting.\n");
-    exit (0);
+    exit(0);
 }
